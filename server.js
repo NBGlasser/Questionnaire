d5ba424ee80433854fc573ff32945f5a61ce9be6
Noah G <NoahBGlasser@gmail.com>
2019-03-31 14:01:23 -0700

    client-side JS debugging

--- server.js
+++ server.js
@@ -15,7 +15,49 @@ app.use('/static', express.static('assets'))
 
 var PORT = process.env.PORT || 3000;
 
-var submissions = []
+var submissions = [{
+    name: "Crispy Hexagons",
+    pic: "https://img.oola.com/filter:scale/slides/2/7/6/5/5/3/2765539557/0797ae845fe1950221f42a24ee94c200327756dd.png?mw=615",
+    q1: 1,
+    q2: 1,
+    q3: 1,
+    q4: 1,
+    q5: 1,
+    q6: 1,
+    q7: 1,
+    q8: 1,
+    q9: 1,
+    q10: 1,
+    },
+    {
+    name: "Pranks",
+    pic: "https://img.oola.com/filter:scale/slides/3/9/9/8/6/3/3998634624/7812ae352a4897d6c7a92e6bd44b3b34d6f0deab.jpeg?mw=615",
+    q1: 2,
+    q2: 2,
+    q3: 2,
+    q4: 2,
+    q5: 2,
+    q6: 2,
+    q7: 2,
+    q8: 2,
+    q9: 2,
+    q10: 2,
+    },
+    {
+    name: "Bunch O' Cinnamon Squares",
+    pic: "https://img.oola.com/filter:scale/slides/3/0/8/4/4/3/3084438369/8911891acf40ae3f2e9d16923fe21858e47050d4.jpeg?mw=615",
+    q1: 3,
+    q2: 3,
+    q3: 3,
+    q4: 3,
+    q5: 3,
+    q6: 3,
+    q7: 3,
+    q8: 3,
+    q9: 3,
+    q10: 3,  
+    }
+]
 
 app.get("/", function(request, response){
     response.sendFile(path.join(__dirname, "questionnaire.html"));
